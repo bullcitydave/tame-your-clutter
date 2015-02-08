2e3583be7da860293efa974a70785b600a4d6bed
Dave Seidman <daveseidman@bosoxdesktop-2.local>
2015-02-08 18:28:16 -0500

    Mod gulp to deploy latest styling changes

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+function fac(r){return 0==r?1:fac(r-1)*r}function sum(r,t){return r+t}function sum(r){for(total=0,i=0;i<r.length;i++)total+=r[i];return total}function range(r,t){if(array=[],t>r)for(;t>=r;)array.push(r),r++;return array}
